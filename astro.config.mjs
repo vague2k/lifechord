5817dba64b9ae8edd1f87f95a3151ee0d446490d
vague2k <ilovedrawing056@gmail.com>
2026-04-06 15:25:50 -0400

    use alpinejs for lightweight interactivity

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,5 +15,6 @@ export default defineConfig({
       },
     },
   },
-});
 
+  integrations: [alpinejs()],
+});
